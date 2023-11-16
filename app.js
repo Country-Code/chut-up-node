228b5a726891a2b59515110757c7d4043a290f02
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2023-11-16 20:06:03 +0100

    add errorMiddleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const userRouter = require("./router/UserRouter")
 const chatRouter = require("./router/ChatRouter")
 const testRouter = require("./router/TestRouter")
 const tools = require("./utils/tools")
+const { errorHandler, notFoundHandler } = require("./middlewares/ErrorMiddleware")
 
 const express = require('express')
 const app = express()
@@ -15,6 +16,8 @@ app.all("*", (req, res, next) => {
 app.use("/api/users", userRouter);
 app.use("/api/chats", chatRouter);
 app.use("/api/tests", testRouter);
+
+app.use(notFoundHandler, errorHandler);
 
 app.listen(process.env.PORT || 5000)
 
