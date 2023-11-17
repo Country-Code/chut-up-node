55bfa4c8968b0f34ddb79e109851b6e7e50e141d
SoufianeMoutaouakkil <moutaouakkilsoufiane@gmail.com>
2023-11-17 11:11:38 +0100

    add cors to the app + fix crypto require in UserModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-
+const crypto = require('crypto');
 const userEntity = require("./entities/UserEntity");
 
 userEntity.methods.verifyPassword = async function (pw) {
