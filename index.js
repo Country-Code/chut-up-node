b1eab9f8964f11c3ed0946701f7df04cd7e98cc8
Karima EL FILALI BOUKAI <karima.elfilaliboukai@alten.com>
2023-11-17 10:31:11 +0100

    add corss

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const env = require("dotenv")
+const cors = require("cors")
 env.config()
 
 const db = require("./config/db")
 db.connect()
 
 const app = require('./app')
+app.use(cors({
+    origin: '*',
+}))
