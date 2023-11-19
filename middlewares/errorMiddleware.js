0765014808d80b5e756adf8b283776473f1a5761
SoufianeMoutaouakkil <soufiane.moutaouakkil.ma@gmail.com>
2023-11-19 01:36:11 +0100

    refactoring errorMiddleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,17 +1,39 @@
+const {logger} = require('../utils/tools');
+
+const getFirstFileFromStack = (stack) => {
+    const indicator = "  at ";
+    const firstIndex = stack.indexOf(indicator);
+    const secondIndex = stack.indexOf("\n", firstIndex + 1);
+
+    return stack.substring(firstIndex + indicator.length, secondIndex);
+}
+
+const prepareAndLogError = (err) => {
+    let message = "";
+    message += `Error Message   : "${err.message}"\n`;
+    message += `Error location  : "${getFirstFileFromStack(err.stack)}"\n`;
+    message += `Error stack     : \n${err.stack}\n`;
+    logger.error().log(message, "Internal Server Error")
+}
+
 const notFoundHandler = (req, res) => {
     res.status(404)
     throw new Error(`Not Found - ${req.originalUrl}`);
 }
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err);
+    prepareAndLogError(err);
 
-    res.status(res.statusCode || 500).json({
-        error: {
-            message: err.message || 'Internal Server Error',
-        },
-        status: "KO"
-    });
+    let message = "An unexpected Internal Server Error occurred. Please check the server logs for more detailed information.";
+    let status = 500;
+
+    if (res.statusCode) {
+        message =  err.message;
+        status = res.statusCode;
+    }
+
+    res.status(status).json({message, status: "KO"});
 }
+
 
 module.exports = { errorHandler, notFoundHandler }
